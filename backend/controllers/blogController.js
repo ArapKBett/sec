b7d379eb3bec45a7464b008d1dc7b6415ed30d85
Arap Bett <kipkorirjob@kabarak.ac.ke>
2025-02-08 02:07:50 -0500

    Create blogController.js

---
+++ backend/controllers/blogController.js
@@ -0,0 +1,38 @@
+const Blog = require('../models/Blog');
+
+// Get all blogs
+exports.getAllBlogs = async (req, res) => {
+  try {
+    const blogs = await Blog.find();
+    res.json(blogs);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+// Get a single blog by ID
+exports.getBlogById = async (req, res) => {
+  try {
+    const blog = await Blog.findById(req.params.id);
+    if (!blog) return res.status(404).json({ message: 'Blog not found' });
+    res.json(blog);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+// Create a new blog
+exports.createBlog = async (req, res) => {
+  const blog = new Blog({
+    title: req.body.title,
+    content: req.body.content,
+    summary: req.body.summary,
+  });
+
+  try {
+    const newBlog = await blog.save();
+    res.status(201).json(newBlog);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
