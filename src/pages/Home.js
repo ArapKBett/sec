04767bd7405ae9f4ad8f6377412158d3ee38fb15
Arap Bett <kipkorirjob@kabarak.ac.ke>
2025-02-08 01:24:54 -0500

    Create Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-4xl font-bold text-center mb-8">Welcome to CyberAware</h1>
+      <p className="text-lg text-center">
+        Learn about cybersecurity and test your knowledge with our interactive quizzes and games.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
