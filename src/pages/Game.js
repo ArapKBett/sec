f91d1826a2c82597eb9c74d1fefbaef372f0a711
Arap Bett <kipkorirjob@kabarak.ac.ke>
2025-02-08 01:53:42 -0500

    Create Game.js

---
+++ src/pages/Game.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Game = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-4xl font-bold text-center mb-8">Cybersecurity Game</h1>
+      <p className="text-lg text-center">Interactive game coming soon!</p>
+    </div>
+  );
+};
+
+export default Game;
